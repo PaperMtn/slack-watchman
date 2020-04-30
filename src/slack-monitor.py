ee85f194e873c82f18b2ff77a4d32a083c2e7a0d
PaperMtn <andrewbyford86@hotmail.com>
2020-04-30 14:16:27 +0100

    Added API calls

--- src/slack-monitor.py
+++ src/slack-monitor.py
@@ -4,6 +4,8 @@ import configparser
 
 
 def get_token():
+    """Get Slack API token from .conf file"""
+    
     conf = configparser.ConfigParser()
     conf.read('../config/keys.conf')
     token = conf.get('auth', 'slack_user_token')
@@ -12,9 +14,30 @@ def get_token():
 
 
 def rate_limit_check(response):
+    """Checks to see whether you have hit the Slack API rate limit.
+    These limits are tiered, so are variable"""
+
     if not response['ok'] and response['error'] == 'ratelimited':
-        print('Slack API rate limit reached')
+        print('Slack API rate limit reached - cooling off')
         time.sleep(60)
+        return True
+    else:
+        return False
+
+
+def week_old(created_time):
+    """Epoch time check for newer than 7 days"""
+
+    if created_time > time.time() - 604800:
+        return True
+    else:
+        return False
+
+
+def month_old(created_time):
+    """Epoch time check for newer than 30 days"""
+
+    if created_time > time.time() - 2592000:
         return True
     else:
         return False
@@ -27,6 +50,8 @@ def write_output(out_path, out_list):
 
 
 def get_users(token):
+    """Return a list of all users in the instance"""
+
     cursor = ''
     results = []
 
@@ -52,12 +77,16 @@ def get_users(token):
 
 
 def get_channels(token):
+    """Return a list of all channels in the instance"""
+
     cursor = ''
     results = []
 
     try:
         r = requests.get('https://slack.com/api/conversations.list',
                          params={'token': token, 'pretty': 1, 'limit': 1, 'cursor': cursor}).json()
+
+        rate_limit_check(r)
 
         if str(r['ok']) == 'False':
             print('END: Unable to dump the channel list. Slack error: ' + str(r['error']))
@@ -67,6 +96,7 @@ def get_channels(token):
                 request_url = 'https://slack.com/api/conversations.list'
                 params = {'token': token, 'pretty': 1, 'limit': 200, 'cursor': cursor}
                 r = requests.get(request_url, params=params).json()
+                rate_limit_check(r)
                 for value in r['channels']:
                     cursor = r['response_metadata']['next_cursor']
                     results.append(value)
@@ -77,6 +107,8 @@ def get_channels(token):
 
 
 def search_files(token, query):
+    """Return a list of all files that match the given query"""
+
     page_count_by_query = {}
     results = []
 
@@ -103,6 +135,8 @@ def search_files(token, query):
 
 
 def search_messages(token, query):
+    """Return a list of all messages that match the given query"""
+
     page_count_by_query = {}
     results = []
 
@@ -129,6 +163,8 @@ def search_messages(token, query):
 
 
 def get_admins(user_list):
+    """Return all admin users from the input userlist"""
+
     results = []
 
     for i in user_list:
@@ -139,6 +175,8 @@ def get_admins(user_list):
 
 
 def get_external_shared(channel_list):
+    """Return all external shared channels from the input channel list"""
+
     results = []
 
     for i in channel_list:
@@ -150,29 +188,33 @@ def get_external_shared(channel_list):
 
 def main():
     token = get_token()
+
+    print(time.time())
+
     # user_list = get_users(token)
-    # channel_list = get_channels(token)
-    file_list = search_files(token, 'bloodhound')
-
-    for i in file_list:
-        print(i)
-
-    message_list = search_messages(token, 'bloodhound')
-
-    for i in message_list:
-        print(i)
-
-
+    channel_list = get_channels(token)
+    # file_list = search_files(token, '.key')
+    #
+    # for i in file_list:
+    #     print(i)
+    #
+    # message_list = search_messages(token, 's3:')
+    #
+    # for i in message_list:
+    #     print(i)
+    #
     # for i in channel_list:
     #     print(i)
 
     # admins = get_admins(user_list)
     # for i in admins:
-    #     print(i['real_name'])
+    #     print(str(i['real_name']) + ' - ' + str(i['id']) + ' - ' + str(i['profile']['email']))
+    #     # print(i)
 
-    # for i in get_external_shared(channel_list):
-    #     print(i)
-
+    for i in get_external_shared(channel_list):
+        if month_old(i['created']):
+            print(i)
+    #
     # for item in user_list:
     #     print(item)
 
